0f36eaecd9c6eaccaa23782bba387ce29da281af
KnoxDev <knoxxus.dev@gmail.com>
2025-08-22 12:45:19 +0530

    Created basic skeleton for backend using Express
    Added cors config, controllers, and routes

---
+++ server/src/config/corsOptions.ts
@@ -0,0 +1,15 @@
+import { CorsOptions } from "cors";
+const allowedOrigins = ["https://localhost:5173"];
+
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}
+
+export default corsOptions;
