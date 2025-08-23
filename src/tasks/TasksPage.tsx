4c9eb53d6ef7d132a91e44384e1490f438439f4b
KnoxDev <knoxxus.dev@gmail.com>
2025-08-23 19:01:05 +0530

    Added loading icons and error messages between API fetch for tasks.

--- src/tasks/TasksPage.tsx
+++ src/tasks/TasksPage.tsx
@@ -6,6 +6,8 @@ import { getTasks } from "../utils/api"
 function TasksPage() {
 
     const [tasks, setTasks] = useState<Task[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [loadError, setLoadError] = useState<string | undefined>(undefined);
 
     const saveTask = (task: Task) => {
         let updatedTasks = tasks.map((t: Task) => {
@@ -15,18 +17,52 @@ function TasksPage() {
     }
 
     useEffect(() => {
-        getTasks()
-            .then(setTasks)
-            .catch(console.error);
+        const fetchTasks = async () => {
+            try {
+                setLoading(true);
+                const data = await getTasks();
+                setLoadError("");
+                setTasks(data);
+            } catch (e) {
+                if (e instanceof Error) {
+                    setLoadError(e.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchTasks();
     }, []);
 
     return (
         <>
             <h1>Tasks</h1>
+
+            {loadError && (
+                <div className="row">
+                    <div className="card large error">
+                        <section>
+                            <p>
+                                <span className="icon-alert inverse "></span>
+                                {loadError}
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            )}
+
             <TaskList
                 tasks={tasks}
                 onSave={saveTask}
             />
+
+            {loading && (
+                <div className="center page">
+                    <span className="spinner primary"></span>
+                    <p>Loading...</p>
+                </div>
+            )}
         </>
     );
 }
