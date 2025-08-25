68c0bec33e19b4b14ae852daf1524671a25dcf35
KnoxDev <knoxxus.dev@gmail.com>
2025-08-25 14:01:45 +0530

    Added functionality for creating a task in the backend server

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -1,11 +1,42 @@
 import { Request, Response } from "express";
 import { MOCK_TASKS } from "../models/MockTasks";
+import Task from "../models/Task";
 
-export const getTasks = (req: Request, res: Response) => {
-    res.status(200).json(MOCK_TASKS);
+export const getTasks = async (req: Request, res: Response) => {
+    try {
+        return res.status(200).json(MOCK_TASKS);
+    } catch (Error) {
+        return res.status(500).json({ error: "Internal Server Error." });
+    }
 }
 
-export const createTask = (req: Request, res: Response) => {
+export const createTask = async (req: Request, res: Response) => {
+    try {
+        const { title, description, assignedTo, dueDate, priority, status, isArchived } = req.body;
 
+        if (!title) {
+            return res.status(400).json({ success: false, error: "Title is required" });
+        }
+
+        const newTask = new Task({
+            id: Date.now(),
+            title,
+            description: description || "",
+            assignedTo: assignedTo || "",
+            dueDate: dueDate ? new Date(dueDate) : new Date(),
+            priority: priority || "medium",
+            status: status || "todo",
+            isArchived: isArchived || false,
+        });
+
+        return res.status(201).json({
+            task: newTask,
+        });
+    } catch (err) {
+        console.error("Error creating task:", err);
+        return res.status(500).json({
+            error: "Internal Server Error",
+        });
+    }
 }
 
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,11 +5,27 @@ const API_URL = "http://localhost:5000/api";
 export async function getTasks(): Promise<Task[]> {
     const response = await fetch(`${API_URL}/`, { method: "GET" });
     if (!response.ok) {
-        throw new Error("Failed to fetch tasks.");
+        throw new Error(`Failed to get tasks: ${response.status} ${response.statusText}`);
     }
     const data = await response.json();
     return data.map((task: Task) => ({
         ...task,
         dueDate: new Date(task.dueDate),
     }));
+}
+
+export async function createTask(task: Task): Promise<Task> {
+    const response = await fetch(
+        `${API_URL}/`,
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(task),
+        });
+    if (!response.ok) {
+        throw new Error(`Failed to create task: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data.task;
 }
