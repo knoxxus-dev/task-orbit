d0467e59dee956d1544d324948508a67a3c39ab2
KnoxDev <knoxxus.dev@gmail.com>
2025-08-28 21:23:58 +0530

    Created a Home Page. Added React Router to navigate between Home Page and Tasks Page using tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import TasksPage from "./tasks/TasksPage";
+import HomePage from "./home/HomePage";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router";
+import logo from "./assets/abstract-logo.png"
 
 function App() {
   return (
-    <div className="container">
-      <TasksPage />
-    </div>
+    <BrowserRouter>
+      <header className="sticky">
+        <span className="logo">
+          <img src={logo} alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/tasks" className="button rounded">
+          Tasks
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tasks" element={<TasksPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
